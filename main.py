5aee8dd1adcd0da3b7eb269e8efb42ad557a53dc
Demontisa <1994769112@qq.com>
2021-06-13 02:40:15 +0800

    已完成获取任务信息并签到

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 import configparser
 import os
 import requests
+import json
 
 #读取配置文件
 root_dir = os.path.abspath(os.curdir)
 configpath = os.path.join(root_dir, "config.ini")
-cf = configparser.ConfigParser()
-cf.read(configpath,encoding='utf-8')
+config = configparser.ConfigParser()
+config.read(configpath,encoding='utf-8')
 
 #读取API接口
-api = cf.get("setting","api")
+api = config.get("setting","api")
 
 #读取md5选项
-md5 = cf.get("setting","md5")
+md5 = config.get("setting","md5")
 
 #读取手机号码
-phone = cf.get("setting","phone")
+phone = config.get("setting","phone")
 
 #读取密码
-password = cf.get("setting","password")
+password = config.get("setting","password")
 
 #登录
 def login():
 
-    if(md5 == "true"):
+    if md5 == "true":
         login = requests.get(api + "/login/cellphone?phone=" + phone + "&md5_password=" + password)
-    login = requests.get(api + "/login/cellphone?phone=" + phone + "&password=" + password)
+    elif md5 == "false":
+        login = requests.get(api + "/login/cellphone?phone=" + phone + "&password=" + password)
 
     #返回cookie
     if login.status_code == 200:
@@ -34,5 +36,35 @@ def login():
 
 
 login_cookie = login()
-print(login_cookie)
+#print(login_cookie)
 
+#获取任务userMissionId和period
+def get_task():
+
+    global userMissionId
+    global period
+
+    get_task = requests.get(api + "/musician/tasks",cookies=login_cookie)
+
+    if get_task.status_code == 200:
+        
+        task_json = get_task.json()
+        #print(jsonstr)
+
+        json_array = json.loads(json.dumps(json.loads(json.dumps(task_json))['data']['list']))[7]
+        #print(json_array)
+        userMissionId = str(json_array['userMissionId'])
+        print("获取missionId成功！")
+
+        period = str(json_array['period'])
+        print("获取period成功！")
+
+get_task()
+
+#签到
+def check():
+    check = requests.get(api + "/musician/cloudbean/obtain?id=" + userMissionId + "&period=" + period,cookies=login_cookie)
+    if check.status_code == 200:
+        print("签到成功！")
+
+check()
